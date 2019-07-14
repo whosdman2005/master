53dc89f5fc08d595db287c7ce5824bed27a5c5f4
hob <whosdman2005@gmail.com>
2019-07-13 23:55:36 -0700

    My First Challenge to HackerRank.com

---
+++ GIT_DEV_python/HackerRank/FirstChallenge.py
@@ -0,0 +1,25 @@
+
+# Complete the function solveMeFirst to compute the sum of two integers.
+
+# Function prototype:
+
+# int solveMeFirst(int a, int b);
+
+# where,
+
+# a is the first integer input.
+# b is the second integer input
+# Return values
+
+# sum of the above two integers
+
+# The sum of the two integers  and  is computed as: 2 + 3 = 5.
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+
+    return a + b
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
